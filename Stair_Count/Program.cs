c3d50aefb19ecda6358b679a50728fd4c9dc57c5
Lasha-Davituliani <davituliani88@gmail.com>
2023-09-27 17:00:40 +0400

    Add project files.

---
+++ Stair_Count/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Stair_Count
+{
+    public class StaircaseClimb
+    {
+        public static int CountVariants(int stairCount)
+        {
+            if (stairCount <= 1)
+            {
+                return 1;
+            }
+
+            return CountVariants(stairCount - 1) + CountVariants(stairCount - 2);
+        }
+
+        public static void Main(string[] args)
+        {
+            int stairCount = 15;
+            int variants = CountVariants(stairCount);
+
+            Console.WriteLine($"Number of ways to climb {stairCount} stairs: {variants}");
+        }
+    }
+}
+
